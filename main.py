d68f4815e270681c005286b92636c8532912efbe
PGescher <paul.gescher@gmail.com>
2025-06-05 22:02:02 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+
+from TemplateCompetitor.my_competitor import MyCompetitor
+from vgc2.net.server import RemoteCompetitorManager, BASE_PORT
+
+
+def main(_args):
+    _id = _args.id
+    competitor = MyCompetitor(name=f"Example {_id}")
+    server = RemoteCompetitorManager(competitor, port=BASE_PORT + _id, authkey=f'Competitor {_id}'.encode('utf-8'))
+    server.run()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', type=int, default=0)
+    args = parser.parse_args()
+    main(args)
