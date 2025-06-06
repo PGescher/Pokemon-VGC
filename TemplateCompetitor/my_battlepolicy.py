20ae92becddb25edd48be271d834504cabc3db50
PGescher <paul.gescher@gmail.com>
2025-06-06 18:42:27 +0200

    Added VGC2 1.0.6 changes to my_battlepolicy.py & Notes

--- TemplateCompetitor/my_battlepolicy.py
+++ TemplateCompetitor/my_battlepolicy.py
@@ -1,16 +1,19 @@
 from itertools import product
 from math import prod
 from random import sample
+from typing import Optional
 
 from numpy import argmax
 from numpy.random import choice
 
 from vgc2.agent import BattlePolicy
-
 from vgc2.battle_engine import State, BattleCommand, calculate_damage, BattleRuleParam, BattlingTeam, BattlingPokemon, \
     BattlingMove, TeamView
 from vgc2.util.forward import copy_state, forward
 from vgc2.util.rng import ZERO_RNG, ONE_RNG
+
+
+# RandomBattlePolicy
 
 class RandomBattlePolicy(BattlePolicy):
     """
@@ -22,7 +25,8 @@ class RandomBattlePolicy(BattlePolicy):
         self.switch_prob = switch_prob
 
     def decision(self,
-                 state: State) -> list[BattleCommand]:
+                 state: State,
+                 opp_view: Optional[TeamView] = None) -> list[BattleCommand]:
         team = state.sides[0].team
         n_switches = len(team.reserve)
         n_targets = len(state.sides[1].team.active)
@@ -37,36 +41,3 @@ class RandomBattlePolicy(BattlePolicy):
                 target = choice(n_switches, p=[1 / n_switches] * n_switches)
             cmds += [(action, target)]
         return cmds
-
-
-# GreedyBattlePolicy
-
-def greedy_single_battle_decision(params: BattleRuleParam,
-                                  state: State) -> list[BattleCommand]:
-    attacker, defender = state.sides[0].team.active[0], state.sides[1].team.active[0]
-    outcomes = [calculate_damage(params, 0, move.constants, state, attacker, defender)
-                if move.pp > 0 and not move.disabled else 0 for move in attacker.battling_moves]
-    return [(int(argmax(outcomes)), 0) if outcomes else (0, 0)]
-
-
-def greedy_double_battle_decision(params: BattleRuleParam,
-                                  state: State) -> list[BattleCommand]:
-    attackers, defenders = state.sides[0].team.active, state.sides[1].team.active
-    strategies = []
-    for sources in product(list(range(len(attackers[0].battling_moves))),
-                           list(range(len(attackers[1].battling_moves))) if len(attackers) > 1 else []):
-        for targets in product(list(range(len(defenders))), list(range(len(defenders)))):
-            damage, ko, hp = 0, 0, [d.hp for d in defenders]
-            for i, (source, target) in enumerate(zip(sources, targets)):
-                attacker, defender, move = attackers[i], defenders[target], attackers[i].battling_moves[source]
-                if move.pp == 0 or move.disabled:
-                    continue
-                new_hp = max(0, hp[target] - calculate_damage(params, 0, move.constants, state, attacker, defender))
-                damage += hp[target] - new_hp
-                ko += int(new_hp == 0)
-                hp[target] = new_hp
-            strategies += [(ko, damage, sources, targets)]
-    if len(strategies) == 0:
-        return [(choice(len(a.battling_moves)), choice(len(defenders))) for a in attackers]
-    best = max(strategies, key=lambda x: 1000 * x[0] + x[1])
-    return list(zip(best[2], best[3]))
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main(_args):
     _id = _args.id
     competitor = MyCompetitor(name=f"Example {_id}")
     server = RemoteCompetitorManager(competitor, port=BASE_PORT + _id, authkey=f'Competitor {_id}'.encode('utf-8'))
+    print(f"[{competitor.name}] Listening on port {BASE_PORT + _id} with authkey 'Competitor {_id}'")
     server.run()
 
 if __name__ == '__main__':
